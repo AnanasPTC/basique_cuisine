07194d34ccb000a6e0fcbd28821a8fa4390a89c9
AnanasPTC <lucas.huchede@gmail.com>
2025-05-24 17:33:09 +0200

    mui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../backend/public',  // Chemin vers le public de Laravel
-    emptyOutDir: false,           // Pour ne pas effacer le public entier
+    emptyOutDir: false,           // Ne pas effacer le public entier
+  },
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@emotion/styled',  // Force l'utilisation d'Emotion
+    },
+  },
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled'],  // Force l'inclusion pour Vite
   },
 })
